df9d041b82488aa320a73736c36d945ec2557597
Lawal Tajudeen O. (The West) <lwltjdn@gmail.com>
2024-01-17 16:51:00 +0100

    Update amenity.py
    updated the amenities class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    ''' Amenity class from the Basemodel '''
+    __tablename__ = 'amenities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place',
+                                        secondary=place_amenities,
+                                        back_populates='amenities')
+    else:
+        name = ''
