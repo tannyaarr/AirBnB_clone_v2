8e815e6f698e9148105ee68bc892f227b1ce016d
tannyaarr <tanyachiras@gmail.com>
2024-01-17 22:36:48 +0000

    setup mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,5 +9,3 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all pivileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
---flush privileges to apply changes
-FLUSH PRIVILEGES;
