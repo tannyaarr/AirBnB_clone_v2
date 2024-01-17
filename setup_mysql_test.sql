5659862a3313cfdf0eae492b08bb99162341898c
tannyaarr <tanyachiras@gmail.com>
2024-01-17 23:58:47 +0000

    changes to some files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create the user hbnb_test if it doesn't exit
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all pivileges
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
