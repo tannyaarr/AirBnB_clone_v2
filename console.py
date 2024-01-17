fd640c3ef96a2db454c6039eb792217723331b3d
Lawal Tajudeen O. (The West) <lwltjdn@gmail.com>
2024-01-17 17:26:13 +0100

    Update console.py

--- console.py
+++ console.py
@@ -208,6 +208,7 @@ class HBNBCommand(cmd.Cmd):
         new_instance.save()
         print(new_instance.id)
 
+
     def help_create(self):
         """Help information for the create method"""
         print("Creates a class of any type")
