4e2929f9e8aa273d37a77a09a61236b79d9096f0
tannyaarr <tanyachiras@gmail.com>
2024-01-19 13:04:34 +0200

    merge file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@
 -- creates the database if it doesn't exist
 -- database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create the user hbnb_test if it doesn't exit
+-- create the user if it doesn't exit
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all pivileges
 GRANT ALL PIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
