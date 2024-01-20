249b3261fac40d61aeed3ee5d747fde300aa7a20
Tajudeen <lwltjdn@gmail.com>
2024-01-20 09:21:36 +0100

    fixed the pep8 issues

--- models/amenity.py
+++ models/amenity.py
@@ -5,10 +5,11 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
+
 class Amenity(BaseModel, Base):
-	''' Amenity class from the Basemodel '''
-	__tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place',
-                                        secondary=place_amenities,
-                                        back_populates='amenities')
+    ''' Amenity class from the Basemodel '''
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False),
+    place_amenities = relationship('Place',
+                                   secondary=place_amenities,
+                                   back_populates='amenities')
