d2dbc2fcd478d57437a44407de68c1e221e728e3
tannyaarr <tanyachiras@gmail.com>
2024-02-01 14:51:24 +0200

    script that generates a tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from fabric.api import local
 from collections.abc import Mapping
 
+
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
