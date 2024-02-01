c78c8cf3c02e3603ca91375e85cfb11a46f4653c
tannyaarr <tanyachiras@gmail.com>
2024-02-01 17:44:18 +0200

    change to file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
-import os.path
+import os.path import exists
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
@@ -10,25 +10,44 @@ env.hosts = ["100.25.179.193", "54.174.161.74"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
+
+def do_pack():
+    """
+    Create a tar gzipped archive of the web_static directory
+    """
+    dt = datetime.utcnow()
+    file_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    local("mkdir -p versions")
+    result = local("tar -czvf versions/{} web_static".format(file_name))
+    if result.failed:
+        return None
+    return "versions/{}".format(file_name)
+
 def do_deploy(archive_path):
-    """Distributes archive to web servers"""
-    if not os.path.exists(archive_path):
+    """
+    Distributes an archive to web servers
+    """
+    if not exists(archive_path):
         return False
+
     try:
         put(archive_path, "/tmp/")
+
         file_name = archive_path.split("/")[-1]
-        file_no_ext = file_name.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(file_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, file_no_ext))
+        folder_name = file_name.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, folder_name))
+
         run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(file_no_ext, file_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(file_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(file_no_ext))
+
+        run("rm -f /data/web_static/current")
+
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(folder_name))
+
+        print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
+
