5c282e4d1202a39e153e3fc51b9a2d73c982d7db
tannyaarr <tanyachiras@gmail.com>
2024-02-21 21:36:45 +0200

    display a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -7,15 +7,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return Hello HBNB"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Return HBNB"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
@@ -23,16 +26,19 @@ def display_c_text(text):
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def display_python_text(text='is cool'):
     """Displays Python followed by the value of the <text>"""
     formatted_text = text.replace('_', ' ')
     return f'Pyhton {formatted_text}'
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """Return a number only if is an integer n"""
     return f'{n} is a number'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
