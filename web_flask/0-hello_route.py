f6dc262e32e1e833b9c756ce391c0ecda530c340
tannyaarr <tanyachiras@gmail.com>
2024-02-21 17:53:55 +0200

    pycodestyle fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,11 @@
     Run the Flask application on 0.0.0.0, port 5000
 """
 
-
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return Hello HBNB"""
@@ -14,4 +15,4 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50i00)
+    app.run(host='0.0.0.0', port=5000)
