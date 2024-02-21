16c64e846d59f4088ac53632baf2d3401a7fc78d
tannyaarr <tanyachiras@gmail.com>
2024-02-21 17:55:03 +0200

    pycodestyle fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,16 +7,18 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return Hello HBNB"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Return the name HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
